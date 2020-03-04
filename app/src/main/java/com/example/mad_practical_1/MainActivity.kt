1640d219cc24d6cabec226e37aeb70931f4fe62c
wweijiann <siewwj-am17"student.tarc.edu.my>
2020-03-04 10:04:38 +0800

    second

--- app/src/main/java/com/example/mad_practical_1/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mad_practical_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.math.BigInteger
@@ -16,26 +17,48 @@ class MainActivity : AppCompatActivity() {
 
         rollButton.setOnClickListener{rollDice()}
         val countButton: Button = findViewById(R.id.button_add)
+        //var diceImage : ImageView? = null
 
         countButton.setOnClickListener{add()}
     }
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
 
         val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
+        lateinit var diceImage : ImageView
+        diceImage = findViewById(R.id.dice_image)
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
 
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
     }
+
     private fun add(){
+        /*
         val resultText: TextView = findViewById(R.id.result_text)
-        var number = resultText.text.toString().toIntOrNull()
-        if(number == null){
+
+        if(resultText.text == "Hello World!"){
             resultText.text == "1";
         }else {
-            number++
-            resultText.text = number.toString()
+            var number = resultText.text.toString().toInt()
+            if (number < 6) {
+                number++
+                resultText.text = number.toString()
+            }
         }
+        */
+
+
+
     }
+
+
 }
